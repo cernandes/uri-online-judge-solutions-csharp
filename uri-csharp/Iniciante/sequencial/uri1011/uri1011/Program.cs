47e12e3646c24980b98e4db4521e8590e44fc092
Claudio <cernandes.silva@gmail.com>
2021-01-09 22:13:10 -0300

    Organização de pastas por temas

---
+++ uri-csharp/Iniciante/sequencial/uri1011/uri1011/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, menor, maior, soma;        
+            while (true)
+            {
+                string[] vet = Console.ReadLine().Split();
+                a = int.Parse(vet[0]);
+                b = int.Parse(vet[1]);
+                if (a <= 0 || b <= 0)
+                {
+                   break;
+                }
+
+                if (a > b)
+                {
+                    maior = a;
+                    menor = b;
+                }
+                else
+                {
+                    maior = b;
+                    menor = a;
+                }
+                soma = 0;
+                for (int i = menor; i <= maior; i++)
+                {
+
+                    Console.Write(i + " ");
+                    soma += i;
+
+                }
+                Console.WriteLine("Sum=" + soma);
+            }
+        }
+    }
+}
